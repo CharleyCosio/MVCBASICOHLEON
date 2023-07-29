de10840eba3c17c7cdde2f64c871c7e96a1c91bf
Charley <esojkarlos3000@gmail.com>
2023-07-29 15:04:31 -0600

    Se anexan los cambios hasta el video numero 6

--- CursoMVC/Controllers/UserController.cs
+++ CursoMVC/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CursoMVC.Models.TableViewModels;
+using CursoMVC.Models.ViewModels;
 using CursoMVC.Models;
 
 namespace CursoMVC.Controllers
@@ -30,5 +31,98 @@ namespace CursoMVC.Controllers
 
             return View(lst);
         }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(UserViewModel model )
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (var db=new cursomvcEntities())
+            {
+                user oUser = new user();
+                oUser.idState = 1;
+                oUser.email = model.Email;
+                oUser.edad = model.Edad;
+                oUser.password = model.Password;
+
+                db.user.Add(oUser);
+                db.SaveChanges();
+            }
+
+            return Redirect(Url.Content("~/User/"));
+
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            EditUserViewModel model = new EditUserViewModel();
+
+            using (var db=new cursomvcEntities())
+            {
+                var oUser = db.user.Find(Id);
+                model.Edad =(int) oUser.edad;
+                model.Email = oUser.email;
+                model.id= oUser.id;
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EditUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (var db = new cursomvcEntities())
+            {
+                var oUser = db.user.Find(model.id);
+                oUser.email = model.Email;
+                oUser.edad = model.Edad;
+   
+                if(model.Password!=null && model.Password.Trim() != "")
+                {
+                    oUser.password = model.Password;
+                }
+
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+            }
+
+
+
+            return Redirect(Url.Content("~/User/"));
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            using (var db = new cursomvcEntities())
+            {
+                var oUser = db.user.Find(Id);
+                oUser.idState = 3;
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+            }
+
+            return Content("1");
+
+        }
+
+
     }
 }
---
+++ CursoMVC/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CursoMVC.Models.ViewModels
+{
+    public class UserViewModel
+    {
+
+
+
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Correo Electronico")]
+        [StringLength(100,ErrorMessage ="La {0} debe tener al menos {1} caracteres",MinimumLength =1)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Contrasenia")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirma Contrasenia")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Las contrasenias no coinciden")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+    }
+
+    public class EditUserViewModel
+    {
+
+        public int id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Correo Electronico")]
+        [StringLength(100, ErrorMessage = "La {0} debe tener al menos {1} caracteres", MinimumLength = 1)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Contrasenia")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirma Contrasenia")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Las contrasenias no coinciden")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+    }
+}
